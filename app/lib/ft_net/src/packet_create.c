043510d59fcaadef343632278c525e250682f2f1
demsasha4yt <demsasha4yt@yandex.ru>
2020-11-07 18:08:26 +0300

    ft_net library started

--- app/lib/ft_net/src/packet_create.c
+++ app/lib/ft_net/src/packet_create.c
@@ -6,14 +6,13 @@
 /*   By: bharrold <bharrold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 16:40:23 by bharrold          #+#    #+#             */
-/*   Updated: 2020/11/07 17:51:49 by bharrold         ###   ########.fr       */
+/*   Updated: 2020/11/07 18:08:19 by bharrold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_net.h"
 
-t_packet		*packet_create(int32_t type, t_sockaddr_in to)
-{
+
 	t_packet	*packet;
 
 	if (!(packet = (t_packet*)malloc(sizeof(t_packet)))) {
